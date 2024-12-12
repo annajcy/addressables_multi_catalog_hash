ae2d2ec762b86a7bd3a8f90b871df7e6d01eba63
jcy <89929647+annajcy@users.noreply.github.com>
2024-12-12 11:38:29 +0800

    modify readme

--- Assets/Script/Load.cs
+++ Assets/Script/Load.cs
@@ -9,63 +9,63 @@ namespace Script
     {
         private void LoadDLC()
         {
+            Caching.ClearCache();
             var cleanBundleCache = Addressables.CleanBundleCache();
             cleanBundleCache.Completed += clearCacheHandle =>
             {
-                if (clearCacheHandle.Status == AsyncOperationStatus.Succeeded)
+                if (clearCacheHandle.Status != AsyncOperationStatus.Succeeded) return;
+                if (!clearCacheHandle.Result) return;
+
+                var dlc1Handle = Addressables.LoadContentCatalogAsync("http://127.0.0.1:8085/StandaloneOSX/DLC1/DLC1_0.1.0.bin");
+                dlc1Handle.Completed += resHandle =>
                 {
-                    if (!clearCacheHandle.Result) return;
-                    var dlc1Handle = Addressables.LoadContentCatalogAsync("http://127.0.0.1:8085/StandaloneOSX/DLC1/DLC1_0.1.0.bin");
-                    dlc1Handle.Completed += resHandle =>
+                    if (resHandle.Status == AsyncOperationStatus.Succeeded)
                     {
-                        if (resHandle.Status == AsyncOperationStatus.Succeeded)
+                        var handle1 = Addressables.LoadAssetAsync<GameObject>("Cube 1");
+                        handle1.Completed += handle =>
                         {
-                            var handle1 = Addressables.LoadAssetAsync<GameObject>("Cube 1");
-                            handle1.Completed += handle =>
-                            {
-                                if (handle.Status == AsyncOperationStatus.Succeeded)
-                                    Instantiate(handle.Result);
-                            };
+                            if (handle.Status == AsyncOperationStatus.Succeeded)
+                                Instantiate(handle.Result);
+                        };
 
-                            var handle4 = Addressables.LoadAssetAsync<GameObject>("Cube 2");
-                            handle4.Completed += handle =>
-                            {
-                                if (handle.Status == AsyncOperationStatus.Succeeded)
-                                    Instantiate(handle.Result);
-                            };
-                        }
-                        else Debug.LogError("catalog load failed: dlc1");
-                    };
-
-                    var dlc2Handle = Addressables.LoadContentCatalogAsync("http://127.0.0.1:8085/StandaloneOSX/DLC2/DLC2_0.1.0.bin");
-                    dlc2Handle.Completed += resHandle =>
-                    {
-                        if (resHandle.Status == AsyncOperationStatus.Succeeded)
+                        var handle4 = Addressables.LoadAssetAsync<GameObject>("Cube 2");
+                        handle4.Completed += handle =>
                         {
-                            var handle2 = Addressables.LoadAssetAsync<GameObject>("Cube 3");
-                            handle2.Completed += handle =>
-                            {
-                                if (handle.Status == AsyncOperationStatus.Succeeded)
-                                    Instantiate(handle.Result);
-                            };
+                            if (handle.Status == AsyncOperationStatus.Succeeded)
+                                Instantiate(handle.Result);
+                        };
+                    }
+                    else Debug.LogError("catalog load failed: dlc1");
+                };
 
-                            var handle3 = Addressables.LoadAssetAsync<GameObject>("Cube 4");
-                            handle3.Completed += handle =>
-                            {
-                                if (handle.Status == AsyncOperationStatus.Succeeded)
-                                    Instantiate(handle.Result);
-                            };
-                        }
-                        else Debug.LogError("catalog load failed: dlc2");
-                    };
-
-                    var handle0 = Addressables.LoadAssetAsync<GameObject>("Cube");
-                    handle0.Completed += handle =>
+                var dlc2Handle = Addressables.LoadContentCatalogAsync("http://127.0.0.1:8085/StandaloneOSX/DLC2/DLC2_0.1.0.bin");
+                dlc2Handle.Completed += resHandle =>
+                {
+                    if (resHandle.Status == AsyncOperationStatus.Succeeded)
                     {
-                        if (handle.Status == AsyncOperationStatus.Succeeded)
-                            Instantiate(handle.Result);
-                    };
-                }
+                        var handle2 = Addressables.LoadAssetAsync<GameObject>("Cube 3");
+                        handle2.Completed += handle =>
+                        {
+                            if (handle.Status == AsyncOperationStatus.Succeeded)
+                                Instantiate(handle.Result);
+                        };
+
+                        var handle3 = Addressables.LoadAssetAsync<GameObject>("Cube 4");
+                        handle3.Completed += handle =>
+                        {
+                            if (handle.Status == AsyncOperationStatus.Succeeded)
+                                Instantiate(handle.Result);
+                        };
+                    }
+                    else Debug.LogError("catalog load failed: dlc2");
+                };
+
+                var handle0 = Addressables.LoadAssetAsync<GameObject>("Cube");
+                handle0.Completed += handle =>
+                {
+                    if (handle.Status == AsyncOperationStatus.Succeeded)
+                        Instantiate(handle.Result);
+                };
             };
         }
 
